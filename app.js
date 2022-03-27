580c0d42d2fdf68a3855682b478b81467a66efd8
Julian Lopez <jlopezsa@gmail.com>
2022-03-26 20:01:44 -0500

    feat: Connection wit mongoo data base

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const configExpress = require('./config/express');
 const routes = require('./routes');
 
@@ -8,6 +10,9 @@ const app = express();
 
 configExpress(app);
 routes(app);
+
+const url = 'mongodb+srv://jlopezsa:veU1RfOrAevnUwSh@julianlopez.teizv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(url);
 
 const port = process.env.PORT || 3030;
 
