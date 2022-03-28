4dd2625c02d291d2c337a04140c2eb9b3276aa69
lmportoesq <lmportoesq@gmail.com>
2022-03-27 20:15:11 -0500

    create user.model.js file

---
+++ api/users/users.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    uppercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    uppercase: true,
+  },
+  timestamps: true,
+  versionKey: false,
+});
+
+module.exports = mongoose.model('User', UserSchema);
