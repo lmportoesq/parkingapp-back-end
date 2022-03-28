d135baf30c59fc9af93dd237af14be4088f2a343
lmportoesq <lmportoesq@gmail.com>
2022-03-27 20:27:55 -0500

    add users route

--- routes.js
+++ routes.js
@@ -1,12 +1,10 @@
-/**
- * Main application routes
- */
-
 const parkings = require('./api/parkings');
+const users = require('./api/users');
 
 function routes(app) {
   // API Routes
   app.use('/api/parkings', parkings);
+  app.use('/api/users', users);
 }
 
 module.exports = routes;
